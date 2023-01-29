4751d756b52987e0683c9da885146b6806c4e150
gitleej <leej95@163.com>
2023-01-29 17:37:54 +0800

    Initial commit

---
+++ Utilities/include/LinkedList.h
@@ -0,0 +1,65 @@
+//
+// Created by AILEE on 2023/1/29.
+//
+
+#ifndef DATASTRUCTURECPP_LINKEDLIST_H
+#define DATASTRUCTURECPP_LINKEDLIST_H
+
+
+namespace ailee {
+    /**
+     * @brief   define list node data struct
+     */
+    typedef struct ListNodeType {
+        int             m_nValue;
+        ListNodeType*   m_pNext;
+    } ListNode;
+
+    /**
+     * @brief   define linked list class
+     */
+    class LinkedList {
+    public:
+        /**
+         * @brief   create a list node.
+         * @param value the node value.
+         * @return  ListNode pointer.
+         */
+        static ListNode* CreateListNode(int value);
+
+        /**
+         * @brief   connect two list node.
+         * @param pCurrent the current list node.
+         * @param pNext the next list node.
+         */
+        static void ConnectListNodes(ListNode* pCurrent, ListNode* pNext);
+
+        /**
+         * @brief   print the value of input list node.
+         * @param pNode the list node that want to print the value.
+         */
+        static void PrintListNode(ListNode* pNode);
+
+        /**
+         * @brief   print the values of list
+         * @param pHead the head pointer of linked list.
+         */
+        static void PrintList(ListNode* pHead);
+
+        /**
+         * @brief   destroy linked list.
+         * @param pHead the head pointer of linked list.
+         */
+        static void DestroyList(ListNode* pHead);
+
+        /**
+         * @brief   Add the node to the end of the linked list.
+         * @param pHead the head pointer of linked list.
+         * @param value the value of the node that wanted to add to the end of the linked list.
+         */
+        static void Add2Tail(ListNode* pHead, int value);
+    };
+}
+
+
+#endif //DATASTRUCTURECPP_LINKEDLIST_H
---
+++ Utilities/src/LinkedList.cpp
@@ -0,0 +1,29 @@
+//
+// Created by AILEE on 2023/1/29.
+//
+
+#include "LinkedList.h"
+
+ailee::ListNode *ailee::LinkedList::CreateListNode(int value) {
+    return nullptr;
+}
+
+void ailee::LinkedList::ConnectListNodes(ailee::ListNode *pCurrent, ailee::ListNode *pNext) {
+
+}
+
+void ailee::LinkedList::PrintListNode(ailee::ListNode *pNode) {
+
+}
+
+void ailee::LinkedList::PrintList(ailee::ListNode *pHead) {
+
+}
+
+void ailee::LinkedList::DestroyList(ailee::ListNode *pHead) {
+
+}
+
+void ailee::LinkedList::Add2Tail(ailee::ListNode *pHead, int value) {
+
+}
