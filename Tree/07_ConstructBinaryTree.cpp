367bdf6d939ffb084403a0c87cf6dc3c9c77abe3
gitleej <leej95@163.com>
2023-02-01 22:40:08 +0800

    07_ConstructBinaryTree

--- Tree/07_ConstructBinaryTree.cpp
+++ Tree/07_ConstructBinaryTree.cpp
@@ -2,3 +2,22 @@
 // Created by AILEE on 2023/2/1.
 //
 
+#include "Tree.h"
+
+#include <iostream>
+
+void test(const char*testName, ailee::TreeNode *pRoot) {
+    std::cout << testName << std::endl;
+
+    // TODO:
+}
+
+void test_01() {
+    test("test 01", nullptr);
+}
+
+int main(int argc, char** argv) {
+    test_01();
+
+    return 0;
+}
