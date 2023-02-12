13934c81cdb32f570d767d5ad6535ea7add88c3d
gitleej <leej95@163.com>
2023-02-12 14:25:03 +0800

    04_FindInPartiallySortedMatrix

--- Array/04_FindInPartiallySortedMatrix.cpp
+++ Array/04_FindInPartiallySortedMatrix.cpp
@@ -13,7 +13,19 @@ bool FindInPartiallySortedMatrix(int* matrix, int rows, int cols, int num) {
         return false;
     }
 
+    int row = 0;
+    int col = cols - 1;
+    while (row < rows && col >= 0) {
+        if (matrix[row * cols + col] > num) {
+            col--;
+        } else if (matrix[row * cols + col] == num) {
+            return true;
+        } else {
+            row++;
+        }
+    }
 
+    return false;
 }
 
 /**
@@ -26,6 +38,12 @@ bool FindInPartiallySortedMatrix(int* matrix, int rows, int cols, int num) {
  */
 void test(const char* testName, int* matrix, int rows, int cols, int num) {
     cout << testName << endl;
+
+    if (FindInPartiallySortedMatrix(matrix, rows, cols, num)) {
+        cout << "true" << endl;
+    } else {
+        cout << "false" <<endl;
+    }
 }
 
 /**
@@ -35,6 +53,58 @@ void test_01() {
     test("test 01", nullptr, 0, 0, 0);
 }
 
+/**
+ * @brief 要查找的数在数组中
+ */
+void test_02() {
+    const int rows = 4;
+    const int cols = 4;
+    int matrix[rows][cols] = {{1, 2, 8, 9}, {2, 4, 9, 12}, {4, 7, 10, 13}, {6, 8, 11, 15}};
+
+    test("test 02", (int *)matrix, rows, cols, 7);
+}
+
+/**
+ * @brief 要查找的数不在数组中
+ */
+void test_03() {
+    const int rows = 4;
+    const int cols = 4;
+    int matrix[rows][cols] = {{1, 2, 8, 9}, {2, 4, 9, 12}, {4, 7, 10, 13}, {6, 8, 11, 15}};
+
+    test("test 03", (int *)matrix, rows, cols, 16);
+}
+
+/**
+ * @brief 要查找的数是最小数字
+ */
+void test_04() {
+    const int rows = 4;
+    const int cols = 4;
+    int matrix[rows][cols] = {{1, 2, 8, 9}, {2, 4, 9, 12}, {4, 7, 10, 13}, {6, 8, 11, 15}};
+
+    test("test 04", (int *)matrix, rows, cols, 1);
+}
+
+/**
+ * @brief 要查找的数是最大数字
+ */
+void test_05() {
+    const int rows = 4;
+    const int cols = 4;
+    int matrix[rows][cols] = {{1, 2, 8, 9}, {2, 4, 9, 12}, {4, 7, 10, 13}, {6, 8, 11, 15}};
+
+    test("test 05", (int *)matrix, rows, cols, 15);
+    test("test 06", (int *)matrix, rows, cols, 0);
+    test("test 07", (int *)matrix, rows, cols, 17);
+}
+
 int main(int argc, char** argv) {
+    test_01();
+    test_02();
+    test_03();
+    test_04();
+    test_05();
+
     return 0;
 }
