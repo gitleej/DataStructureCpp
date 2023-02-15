3043b9a12e9407209f255e864a0b5598cd4aebcb
gitleej <leej95@163.com>
2023-02-15 18:32:08 +0800

    add 38_StringPermutation

---
+++ String/38_StringPermutation.cpp
@@ -0,0 +1,22 @@
+//
+// Created by AILEE on 2023/2/15.
+// 题目38：字符串的排列
+// 题目：输入一个字符串，打印出该字符串中字符的所有排列，例如：输入字符串abc，则打印出abc，acb，bac，bca，cab，cba
+//
+
+#include <iostream>
+
+using namespace std;
+
+void test(const char *testName, char *str) {
+
+}
+
+void test_01() {
+
+}
+
+int main(int argc, char** argv) {
+
+    return 0;
+}
